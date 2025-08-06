598629795f1760480523ff9c88cb593be7941eae
Nitesh Kesarkar <nitesh.kesarkar@codeblaze.ae>
2025-08-06 12:00:24 +0530

    feat(map): Integrate google map

--- src/routes/router.tsx
+++ src/routes/router.tsx
@@ -4,47 +4,59 @@ import {
   RootRoute,
   Outlet,
   Navigate,
-} from '@tanstack/react-router';
+} from "@tanstack/react-router";
 
-import TodoPage from '../features/todos/presentation/screens/TodoPage';
-import LoginPage from '../features/auth/presentation/screens/LoginPage';
+import TodoPage from "../features/todos/presentation/screens/TodoPage";
+import LoginPage from "../features/auth/presentation/screens/LoginPage";
+import { MapPage } from "../features/map/MapPage";
+import LanguageSelector from "../shared/components/LanguageSelector";
 
-
-declare module '@tanstack/react-router' {
+declare module "@tanstack/react-router" {
   interface Register {
-    router: typeof router
+    router: typeof router;
   }
 }
 
 const rootRoute = new RootRoute({
   component: () => {
     return (
-      <div>
+      <>
+        <LanguageSelector />
         <Outlet />
-      </div>
+      </>
     );
   },
 });
 
 const indexRoute = createRoute({
   getParentRoute: () => rootRoute,
-  path: '/',
+  path: "/",
   component: () => <Navigate to="/login" />,
-})
+});
 
 const loginRoute = createRoute({
   getParentRoute: () => rootRoute,
-  path: 'login',
+  path: "login",
   component: LoginPage,
-})
+});
 
 const todosRoute = createRoute({
   getParentRoute: () => rootRoute,
-  path: 'todos',
+  path: "todos",
   component: TodoPage,
-})
+});
 
-const routeTree = rootRoute.addChildren([indexRoute, todosRoute, loginRoute]);
+const mapsRoute = createRoute({
+  getParentRoute: () => rootRoute,
+  path: "maps",
+  component: MapPage,
+});
+
+const routeTree = rootRoute.addChildren([
+  indexRoute,
+  todosRoute,
+  loginRoute,
+  mapsRoute,
+]);
 
 export const router = createRouter({ routeTree });
-
