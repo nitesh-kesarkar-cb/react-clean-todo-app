d9772787614bd2caba1f14411ad236259e7fec76
Nitesh Kesarkar <nitesh.kesarkar@codeblaze.ae>
2025-08-06 11:10:33 +0530

    feat(i18n): Integrate i18n for internationalisation

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,17 @@
 import { RouterProvider } from '@tanstack/react-router';
 import { router } from './routes/router';
 import { withSentryBoundary } from './shared/hoc/withSentryBoundary';
+import './i18n';
 import './App.css'
+import LanguageSelector from './shared/components/LanguageSelector';
 
 const WrappedRouterProvider = withSentryBoundary(RouterProvider);
 
 function App() {
-  return <WrappedRouterProvider router={router} />;
+  return <>
+    <LanguageSelector />
+    <WrappedRouterProvider router={router} />
+  </>;
 }
 
 export default App;
-
