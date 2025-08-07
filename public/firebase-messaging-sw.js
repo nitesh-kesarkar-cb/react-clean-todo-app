553b26d940a37b9a1633bfe5af9c27d184e6fdbe
Nitesh Kesarkar <nitesh.kesarkar@codeblaze.ae>
2025-08-07 14:15:06 +0530

    minor fix

--- public/firebase-messaging-sw.js
+++ public/firebase-messaging-sw.js
@@ -12,7 +12,15 @@ importScripts(
     'https://www.gstatic.com/firebasejs/10.7.1/firebase-messaging-compat.js'
 )
 
-const firebaseConfig = JSON.parse(import.meta.env.VITE_FIREBASE_CONFIG)
+const firebaseConfig = {
+    apiKey: 'AIzaSyBeoMAGOb02Fsd0lvfROREiGURh1ovWv3Q',
+    authDomain: 'fir-test-react-468305.firebaseapp.com',
+    projectId: 'firebase-test-react-468305',
+    storageBucket: 'firebase-test-react-468305.firebasestorage.app',
+    messagingSenderId: '386155533989',
+    appId: '1:386155533989:web:bf4345b34fc41f355e9f4c',
+    measurementId: 'G-8KZZ7Q2F5F',
+}
 
 firebase.initializeApp(firebaseConfig)
 
